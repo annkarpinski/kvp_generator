02232aac779a6a99b0ab2748b9e2050f49e15e13
Ann Karpinski <annkarpinski@gmail.com>
2020-12-08 14:54:06 -0700

    generates string of two KVPs

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function generateKVP() {
-  var txtName = document.getElementById("txtName");
+  var txtName1 = document.getElementById("txtName1");
+  var txtName2 = document.getElementById("txtName2");
   var txtOutput = document.getElementById("txtOutput");
 
-  var name = txtName.value;
-  txtOutput.value = "custom[" + name + "]";
+  var name1 = txtName1.value;
+  var name2 = txtName2.value;
+
+  if (name2 === "") {
+    txtOutput.value = "custom[" + name1 + "]";
+  } else if (name2 !== "") {
+    txtOutput.value = "custom[" + name1 + "]&custom[" + name2 + "]";
+  }
 }
